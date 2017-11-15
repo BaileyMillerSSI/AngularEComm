87507241d6be90820a0721ec41b933a10915de74
Bailey Miller <bmiller@susquehannasoftware.com>
2017-11-15 16:06:03 -0500

    Looking good.

--- WebStore/WebStore/ClientApp/app/components/mini/productcard.component.ts
+++ WebStore/WebStore/ClientApp/app/components/mini/productcard.component.ts
@@ -1,10 +1,12 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'product-card',
+    styleUrls: ['./productcard.component.css'],
     templateUrl: './productcard.component.html'
 })
 export class ProductCardComponent {
 
+    @Input() Title: string;
 
 }
